22b3065c20967b8ee2711ebd3a5c09c482f6eed5
JoanneOs <jowan.awsman@yahoo.com>
2025-03-20 14:57:25 -0500

    prit difference

--- solarsystem.js
+++ solarsystem.js
@@ -233,3 +233,6 @@ console.log(sum);
   // 6. Find the difference between Jupiter's momentum and Pluto's momentum. (To get momentum, you'll want the product of velocity and mass).
 
   let jupiterMomentum = system.jupiter.velocity * system.jupiter.mass;
+  let plutoMomentum = system.pluto.velocity * system.pluto.mass;
+  let momentumDifference = jupiterMomentum - plutoMomentum;
+  console.log(momentumDifference);
